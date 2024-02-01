c59d64b21a0a6a8a82897e3a7bc7f3314774bc5f
sandeep14k <143644790+sandeep14k@users.noreply.github.com>
2024-02-02 00:28:24 +0530

    Add files via upload

---
+++ cf_project/worker/worker.go
@@ -0,0 +1,54 @@
+package worker
+
+import (
+	"CF_PROJECT/cfapi"
+	"CF_PROJECT/models"
+	"CF_PROJECT/store"
+	"fmt"
+	"log"
+	"sync"
+	"time"
+)
+
+func PerformWork(m *store.MongoStore, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		m.OpenConnectionWithMongoDB()
+		obj := new(cfapi.CodeforcesClient)
+		RecentActions, err := obj.RecentActions(100)
+		if err != nil {
+			fmt.Println("error occurred")
+			return
+		}
+
+		maxTimeStamp, err := m.GetMaxTimeStamp()
+		if err != nil {
+			log.Printf("Error while getting maxTimeStamp: %v", maxTimeStamp)
+		}
+
+		log.Printf("Got maxTimeStamp successfully")
+
+		var NewData []models.RecentAction
+
+		for i := 0; i < len(RecentActions); i++ {
+			if RecentActions[i].TimeSeconds > int64(maxTimeStamp) {
+				NewData = append(NewData, RecentActions[i])
+			}
+		}
+
+		log.Printf("RecentActions stored in NewData successfully ")
+
+		err = m.StoreRecentActionsInTheDatabase(NewData)
+		if err != nil {
+			log.Printf("Error occurred while storing data : %v", err)
+			return
+		}
+
+		var temp []models.RecentAction
+
+		NewData = temp
+
+		log.Printf("The worker will sleep for 5 min now.")
+		time.Sleep(5 * time.Minute)
+	}
+}
